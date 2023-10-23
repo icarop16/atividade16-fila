77621f0254a70b168db225dea66ed2e5b6d6a2ba
icarop16 <pedroicaro161107@gmail.com>
2023-10-23 14:22:23 -0300

    Atividade Fila

---
+++ pedroicaro.py
@@ -0,0 +1,6 @@
+# Para ter acesso a fila de prioridade, você deve ser idoso, gestante ou cadeirante. 
+# Escreva um programa que pergunta a situação do usuário 
+# (se é idoso, se é gestante, se é cadeirante ou nenhum destes) e diga se ele pode ter acesso a fila prioridade ou não.
+print("Classificações:")
+print("1-Criança, 2-Adolescente, 3-Adulto, 4-Idoso, 5-Gestante e 6-Cadeirante ")
+Prioridade = input("Em  qual situação você se enconta?")
